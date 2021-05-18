a25a79280eb56c717faf9921500ebb7873a5b213
Alessandro Silva <alessandro-psilva@outlook.com>
2021-05-18 18:58:43 -0300

    apsilva_bot.py: add (inclsuão de curtidas no instagram e passagem de quantidade de paginas)

--- apsilva_bot/src/apsilva_bot.py
+++ apsilva_bot/src/apsilva_bot.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import random
+import os
 
 
 
@@ -11,6 +12,8 @@ class ApsilvaBot:
         self.password = password
         self.driver   = webdriver.Firefox(
             executable_path='geckodriver\geckodriver.exe')
+
+        os.system("cls")
         
     def login(self):
         driver = self.driver
@@ -44,14 +47,43 @@ class ApsilvaBot:
         driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
         time.sleep(5)
 
-        for pagina in range(1, 5):
-            script = "window.scrollTo(0, document.body.scrollHeight);"
+        script = "window.scrollTo(0, document.body.scrollHeight);"
+
+        qtde_pag = int(input('qtde_paginas: '))
+        for pagina in range(1, qtde_pag):
             driver.execute_script(script)
             time.sleep(3)
 
         row_hrefs = driver.find_elements_by_tag_name("a")
         pic_hrefs = [href.get_attribute("href") for href in row_hrefs]
-        print(hashtag + " fotos: " + str(len(pic_hrefs)))
+
+        qtde_links     = len(pic_hrefs)
+        qtde_links_erro  = 0
+        qtde_links_sucesso  = 0
+
+        for href in pic_hrefs:
+            try:
+                href.index("https://www.instagram.com/p")
+                qtde_links_sucesso += 1
+            except ValueError as err:
+                qtde_links_erro += 1
+                continue
+
+            driver.get(href)
+
+            try:
+                time.sleep(random.randint(19, 23))  
+                driver.execute_script(script)              
+                driver.find_element_by_xpath(
+                    '//span[@class="fr66n"]').click()
+            except Exception as e:
+                print(e)
+                time.sleep(5)
+
+            os.system("cls")
+            print('Total de links recuperados {}'.format(qtde_links))
+            print('Total de links com sucesso {}'.format(qtde_links_sucesso))
+            print('Total de links com erro {}'.format(qtde_links_erro))      
 
 if __name__ == '__main__':
     apsilva_bot = ApsilvaBot(
